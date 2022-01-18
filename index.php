7f80ded1f782dbd3a9d914ef72b44f2540de6fe4
Mehedy-Hasan <mehedyandhasan@gmail.com>
2022-01-18 23:15:28 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+// Comment these lines to hide errors
+error_reporting(E_ALL);
+ini_set('display_errors', 0);
+
+require 'includes/config.php';
+require 'includes/functions.php';
+//This Change is made by Md. Mehedy Hasan
+//Assignment 01
+//DevSkill DevOps
+
+init();
